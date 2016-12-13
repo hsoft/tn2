102aeb43e984e7f2b9f04cd4b86581cf05742c1e
Virgil Dupras <hsoft@hardcoded.net>
2016-12-12 20:32:43 -0500

    Formattage de la liste d'articles

--- src/tn2app/templatetags/tn2.py
+++ src/tn2app/templatetags/tn2.py
@@ -1,5 +1,6 @@
 from django import template
 from django.templatetags.static import static
+from django.utils.safestring import mark_safe
 
 from easy_thumbnails.files import get_thumbnailer
 
@@ -19,3 +20,12 @@ def article_thumbnail(article):
     else:
         return static('images/image-placeholder.png')
 
+@register.filter()
+def user_link(user):
+    if not user:
+        return ''
+    elif user.profile:
+        # TODO: is this really safe? is display_name properly bleachified?
+        return mark_safe('<a href="{}">{}</a>'.format(user.profile.get_absolute_url(), user.profile.display_name))
+    else:
+        return str(user)
