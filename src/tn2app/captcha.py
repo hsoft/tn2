66996658e29e16bf9525c89cff13e40c81f40176
Virgil Dupras <hsoft@hardcoded.net>
2017-05-19 14:30:31 -0400

    Fix d'erreurs de captcha trop long
    La longueur maximale des défis dans django-simple-catcha est de 32
    caractères. Ça n'arrive pas souvent, mais ça arrive...

--- src/tn2app/captcha.py
+++ src/tn2app/captcha.py
@@ -5,5 +5,7 @@ from num2words import num2words
 def numerical_string_challenge():
     number = random.randrange(1000)
     s = num2words(number, lang='fr')
+    if len(s) > 32: # maximum challenge length in django-simple-captcha is 32
+        return numerical_string_challenge()
     return s, str(number)
 
