eb6c96ecf8b17060457f371c66985577259ed997
Virgil Dupras <hsoft@hardcoded.net>
2017-09-25 13:18:17 -0400

    Réorganisation du provisioning ansible

--- ansible/roles/tn2/templates/develop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "You can open the debug site at:"
-echo "http://{{ django_domain_name }}:8080"
-
-export TN2_CONF_PATH="{{ django_jsonsettings_path }}"
-cd {{ django_src_path }}
-{{ django_env }}/bin/python manage.py runserver 0.0.0.0:8080
--- src/tn2app/management/commands/load_initial_data.py
+++ src/tn2app/management/commands/load_initial_data.py
@@ -5,10 +5,13 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db.models.signals import post_save
 
-from ...models import DiscussionComment
+from ...models import DiscussionComment, Project
 
 class Command(BaseCommand):
     def handle(self, *app_labels, **options):
+        if Project.objects.exists():
+            print("We already have data. Skip load_initial_data.")
+            return
         assert post_save.disconnect(sender=DiscussionComment, dispatch_uid='discussioncomment_was_saved')
         FIXTURES = [
             'pages',
